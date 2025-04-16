779ae8baea0c8219e964b9dcd8110b2415616bde
trevor-hunter <hunt.trev.99@gmail.com>
2025-04-16 17:49:34 -0600

    created components for all app pages, removed placeholder content from app.component.html and added a nav bar with routing to each created component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MapComponent } from './map/map.component';
+import { ItineraryComponent } from './itinerary/itinerary.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home-component', component: HomeComponent },
+    { path: 'map-component', component: MapComponent },
+    { path: 'itinerary-component', component: ItineraryComponent },
+    { path: 'gallery-component', component: GalleryComponent },
+    { path: 'about-component', component: AboutComponent }
+];
